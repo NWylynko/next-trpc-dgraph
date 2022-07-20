8d28ee2a9855d63b7d71248ddd026e4d1b2b3ea1
Nick Wylynko <nick.wylynko@navitas.com>
2022-07-20 15:58:19 +0800

    Fix build issues

--- server/router/helloworld/methods.ts
+++ server/router/helloworld/methods.ts
@@ -1,4 +1,4 @@
 import { createMethods } from "../createMethods";
 import { getSdk } from "./methods.generated";
 
-export const getMethods = createMethods(getSdk);
+export const methods = createMethods(getSdk);
--- server/router/helloworld/router.ts
+++ server/router/helloworld/router.ts
@@ -1,6 +1,6 @@
 import { t } from "../trpc";
 import { z } from "zod";
-import { getMethods } from "./methods";
+import { methods } from "./methods";
 
 export const helloRouter = t.router({
   me: t.procedure
@@ -9,5 +9,13 @@ export const helloRouter = t.router({
     }))
     .query(async ({ input }) => {
       return { hello: input.name }
+    }),
+  id: t.procedure
+    .input(z.object({
+      id: z.string()
+    }))
+    .query(async ({ input }) => {
+      const result = await methods.getUser({ userId: input.id })
+      return result.getUser;
     })
 });
